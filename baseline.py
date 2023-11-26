462a0dcedbc818e9804df719258c4e44eb277b60
rowhan1111 <sam.han706@gmail.com>
2023-11-26 20:39:45 +0900

    cross validation mean for each fold

--- baseline.py
+++ baseline.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 from scipy import stats
 from tqdm import tqdm
 from sklearn.model_selection import KFold
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_score, cross_val_predict
 # Information
 # - Baseline: No PCA, SVM
 # - Dataset: get_weighted_distribution() 함수 적용
@@ -27,32 +27,42 @@ X = data.drop(columns=[
 y = data['category']
 
 # 훈련 및 테스트 데이터 분할
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
+#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
 
 # 데이터 표준화
 scaler = StandardScaler()
-X_train_scaled = scaler.fit_transform(X_train)
-X_test_scaled = scaler.transform(X_test)
+#X_train_scaled = scaler.fit_transform(X_train)
+#X_test_scaled = scaler.transform(X_test)
 
 # SVM 모델 훈련 및 평가
 svm_model = SVC()
-svm_model.fit(X_train_scaled, y_train)
+#svm_model.fit(X_train_scaled, y_train)
 kf = KFold(n_splits=3, shuffle=True, random_state=42)  #Cross Validation for k = 3
 # Perform k-fold cross-validation
-cv_results = cross_val_score(svm_model, X, y, cv=kf)#getting cross validation scores
-#pca_scores[i, j] = accuracy_score(y_test, pca_y_pred_test)
+#cv_results_score = cross_val_score(svm_model, X_scaled, y, cv=kf)#getting cross validation scores
+# Initialize an array to store predicted values
+all_y_pred = np.array([])
+scores = np.array([[], [], [], []])
+# Iterate over each fold
+for train_index, test_index in kf.split(X):
+    X_train, X_test = X.iloc[train_index], X.iloc[test_index]
+    X_train_Scaled = scaler.fit_transform(X_train)
+    X_test_Scaled = scaler.transform(X_test)
+    y_train, y_test = y.iloc[train_index], y.iloc[test_index]
 
-y_pred_test = svm_model.predict(X_test_scaled)
+    # Fit the classifier on the training data
+    svm_model.fit(X_train_Scaled, y_train)
 
-# 성능 측정
-#accuracy = accuracy_score(y_test, y_pred_test)
-accuracy = np.mean(cv_results)
-f1_micro = f1_score(y_test, y_pred_test, average='micro')
-f1_macro = f1_score(y_test, y_pred_test, average='macro')
-f1_weighted = f1_score(y_test, y_pred_test, average='weighted')
-
+    # Get predicted values for the test set in this fold
+    fold_y_pred = svm_model.predict(X_test_Scaled)
+    # Append the predicted values to the array
+    all_y_pred = np.concatenate((all_y_pred, fold_y_pred))
+    # 성능 측정
+    all_scores = np.array([[accuracy_score(y_test, fold_y_pred)], [f1_score(y_test, fold_y_pred, average='micro')], [f1_score(y_test, fold_y_pred, average='macro')], [f1_score(y_test, fold_y_pred, average='weighted')]])
+    scores = np.append(scores, all_scores, axis = 1)
 # 결과 출력
-print(f"Accuracy: {accuracy}")
-print(f"F1 Score (Micro): {f1_micro}")
-print(f"F1 Score (Macro): {f1_macro}")
-print(f"F1 Score (Weighted): {f1_weighted}")
+mean_scores = np.mean(scores, axis = 1)
+print(f"Accuracy: {mean_scores[0]}")
+print(f"F1 Score (Micro): {mean_scores[1]}")
+print(f"F1 Score (Macro): {mean_scores[2]}")
+print(f"F1 Score (Weighted): {mean_scores[3]}")
